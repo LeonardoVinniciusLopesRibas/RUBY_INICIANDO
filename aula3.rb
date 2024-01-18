6568408b7d81d94086f198ebf5ce1158cf999f19
LeonardoVinniciusLopesRibas <110777424+LeonardoVinniciusLopesRibas@users.noreply.github.com>
2024-01-18 14:16:04 -0300

    Add files via upload

---
+++ aula3.rb
@@ -0,0 +1,10 @@
+print "Informe um número: "
+num1 = gets.chomp.to_i
+
+print "Informe outro número: "
+num2 = gets.chomp.to_i
+
+soma = num1+num2
+
+puts "O resultado da soma é #{soma}"
+
---
+++ aula5.rb
@@ -0,0 +1,32 @@
+dia = 'domingo'
+
+if dia == 'domingo' 
+    almoco = 'especial'
+end
+puts "Hoje nosso almoço será #{almoco}"
+
+
+
+dia2 = 'domingo'
+
+if dia2 == 'domingo' 
+    almoco2 = 'especial'
+else
+    almoco2 = 'normal'
+end
+puts "Hoje nosso almoço será #{almoco2}"
+
+
+
+dia3 = 'domingo'
+
+if dia3 == 'domingo' 
+    almoco3 = 'especial'
+elsif dia3 == 'feriado'
+    almoco3 == 'mais tarde'
+else
+    almoco3 = 'normal'
+end
+puts "Hoje nosso almoço será #{almoco3}"
+
+
